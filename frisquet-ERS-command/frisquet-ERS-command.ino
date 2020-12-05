8f04f7f2586d54f9f1d58742a05e742089e4358f
etimou <etienne.mouragnon@gmail.com>
2020-12-05 12:16:52 +0100

    checksum + transmit rewrite

--- frisquet-ERS-command/frisquet-ERS-command.ino
+++ frisquet-ERS-command/frisquet-ERS-command.ino
@@ -1,37 +1,46 @@
 byte ERS_pin = 3;
 int long_pulse = 825;
-byte message[17] = {0x00, 0x00, 0x00, 0x3F, 0x9F, 0xE7, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFD, 0x00, 0xFF, 0x00};
+byte message[17] = {0x00, 0x00, 0x00, 0x7E, 0xBE, 0xE7, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFD, 0x00, 0xFF, 0x80};
 byte old_state, num_byte;
 byte bitstuff = 0;
 
-void pulse (byte state) {
-  digitalWrite(ERS_pin, state);
+void writeBit(bool Bit) {
+  old_state = !old_state;
+  digitalWrite(ERS_pin, old_state);
   delayMicroseconds(long_pulse);
+
+  if (Bit) {
+    old_state = !old_state;
+    digitalWrite(ERS_pin, old_state);
+  }
+  delayMicroseconds(long_pulse);
+
 }
 
 void conversion(byte input) {
   for (byte n = 0; n < 8; n++) { // boucle pour chaque bit
-    if (bitRead(input, n) == 1) bitstuff++;  // incrémente le compteur bitstuffing
-    if ((bitstuff > 5) && (num_byte > 9) && (num_byte < 15)) { // bitstuffing bit 10 à 14
-      pulse(!old_state);
-      pulse(!old_state);
-      old_state = !old_state;
-      bitstuff = 0;
+    writeBit(bitRead(input, n));
+    if (num_byte >= 4 && num_byte <= 14) {
+      if (bitRead(input, n) == 1) bitstuff++;  // incrémente le compteur bitstuffing
     }
+    if (bitRead(input, n) == 0) bitstuff = 0;
 
-    pulse(!old_state);
-    if (bitRead(input, n) == 1) {
-      old_state = !old_state;
-    } else {
+    if (bitstuff >= 5) {
+      writeBit(0);
       bitstuff = 0;
     }
-    pulse(!old_state);
-    old_state = !old_state;
   }
 }
 
 void commande(byte prechauffage, byte chauffage) {
-  if ((chauffage <= 100) and ((prechauffage==0) or (prechauffage==3) or (prechauffage==4))) {
+  Serial.print("Nouvelle commande de chauffage : (");
+  Serial.print(prechauffage);
+  Serial.print(",");
+  Serial.print(chauffage);
+  Serial.println(")");
+  Serial.flush();
+
+  if ((chauffage <= 100) and ((prechauffage == 0) or (prechauffage == 3) or (prechauffage == 4))) {
     for (byte x = 0; x < 3; x++) { // boucle de 3 messages
       old_state = 0;
       message[9] = x;  // numero message : 0 à 2
@@ -41,18 +50,28 @@ void commande(byte prechauffage, byte chauffage) {
         message[10] = prechauffage + 0x80;
       }
       message[11] = chauffage;
-      message[14] = 0x13B-message[9]-message[10]-message[11]-message[12];
+
+      int checksum = 0;
+      for (int i = 4; i <= 12; i++) {
+        checksum -= message[i];
+      }
+      message[13] = highByte(checksum);
+      message[14] = lowByte(checksum);
+
       for (num_byte = 1; num_byte < 17; num_byte++) { // boucle de 16 bytes
         conversion(message[num_byte]);
       }
-      digitalWrite(ERS_pin, HIGH);
+      //digitalWrite(ERS_pin, HIGH);
+      digitalWrite(ERS_pin, LOW);
       delay(33);
     }
     digitalWrite(ERS_pin, LOW);
+
   } else {
     return;
   }
 }
+
 
 void setup() {
   pinMode(ERS_pin, OUTPUT);
