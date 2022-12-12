d0a79f2a311ec37865f93bf0a5abc3fbeddaca6d
Christophe Hardouin Duparc <christophe.hardouin.duparc@gmail.com>
2022-12-12 08:45:58 +0100

    Bascule vers le code de @ParkerLewis
    https://community.jeedom.com/t/domotisation-frisquet-prestige-condensation-eco-radio-system/76054/81?u=christophehd

--- frisquet-ERS-command/frisquet-ERS-command-transceiver.ino
+++ frisquet-ERS-command/frisquet-ERS-command-transceiver.ino
@@ -1,90 +1,23 @@
-#include <SerialTerminal.hpp>
 
-#define TRANSCEIVER_TX_PIN      14    // Set the pin that receives data from your 433.42 Mhz Receiver
+#define TRANSCEIVER_TX_PIN      14    // Set the pin that emits data towards the 433.42 Mhz Transmitter
 #define TRANSCEIVER_MODE_PIN    15    // Aurel transceivers have a pin that let the hardware switch to RX and TX mode
 #define TRANSCEIVER_ENABLE_PIN  22    // Aurel transceivers have a pin that must be set to HIGH to enable the transmitter
 
 // Variables
 int long_pulse = 825;
-byte message[17] = {0x00, 0x00, 0x00, 0x7E, 0xFF, 0xFF, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFD, 0x00, 0xFF, 0x80};
+byte message[17] = {0x00, 0x00, 0x00, 0x7E, 0x??, 0x??, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFD, 0x00, 0xFF, 0x80};
 byte old_state, num_byte;
 byte bitstuff = 0;
 
-void writeBit(bool Bit) {
-  old_state = !old_state;
-  digitalWrite(TRANSCEIVER_TX_PIN, old_state);
-  delayMicroseconds(long_pulse);
+String chaineCaractere = "";
+String mode = "";
+String temperature = "";
+String modeTexte = "" ;
+String tempTexte = "" ;
+String invalide = "INVALIDE";
 
-  if (Bit) {
-    old_state = !old_state;
-    digitalWrite(TRANSCEIVER_TX_PIN, old_state);
-  }
-  else {
-    digitalWrite(TRANSCEIVER_TX_PIN, old_state);
-  }
-  delayMicroseconds(long_pulse);
-}
-
-void conversion(byte input) {
-  for (byte n = 0; n < 8; n++) { // boucle pour chaque bit
-    writeBit(bitRead(input, n));
-    if (num_byte >= 4 && num_byte <= 14) {
-      if (bitRead(input, n) == 1) bitstuff++;  // incrémente le compteur bitstuffing
-    }
-    if (bitRead(input, n) == 0) bitstuff = 0;
-    if (bitstuff >= 5) {
-      writeBit(0);
-      bitstuff = 0;
-    }
-  }
-}
-
-void commande(byte mode, byte temperature) {
-  if ((temperature <= 100) and ((mode==0) or (mode==3) or (mode==4))) {
-    for (byte x = 0; x < 3; x++) { // boucle de 3 messages
-      old_state = 0;
-      
-      // Message 9 --> numero de message 0 à 2
-      message[9] = x;  
-      
-      // Message : 10 -> mode
-      if (x == 2) message[10] = mode;
-      else message[10] = mode + 0x80;
-
-      // Message : 11 -> temperature
-      message[11] = temperature;
-      
-      // Messages 13 et 14 -> checksum
-      int checksum = 0;
-      for (int i = 4; i <= 12; i++) {
-        checksum -= message[i];
-      }
-      message[13] = highByte(checksum);
-      message[14] = lowByte(checksum);
-
-      Serial.print("Trame ");
-      Serial.print(x);
-      Serial.print(" : ");
-      
-      for (int i = 1; i <= 16; i++) {
-        if (message[i] <= 0x0F) Serial.print("0");
-        Serial.print(message[i], HEX);
-      }
-      Serial.println("");
-      
-      for (int i = 1; i <= 16; i++) { // boucle de 16 bytes
-          conversion(message[i]);
-      }
-      
-      digitalWrite(TRANSCEIVER_TX_PIN, LOW);
-      delay(33);
-    }
-    digitalWrite(TRANSCEIVER_TX_PIN, LOW);
-  } else {
-    return;
-  }
-}
-
+// A l'init
+// Setup du chip Aurel
 void setup() {
   // set the Aurel transceiver to RX mode
   pinMode(TRANSCEIVER_MODE_PIN, OUTPUT);
@@ -99,66 +32,154 @@ void setup() {
   // assign an interrupt on pin x on state change
   pinMode(TRANSCEIVER_TX_PIN, OUTPUT);
   digitalWrite(TRANSCEIVER_TX_PIN, LOW);
-  
-  pinMode (LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-   
-  Serial.begin(9600) ;
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.begin(57600);
 }
 
+// En continu
+// Surveillance et émission de trame si commande texte
 void loop() {
 
-  String chaineCaractere = "" ;
-  String mode = "" ;
-  String modeTexte = "" ;
-  String temperature = "" ;
-  char   acquisDonnees = "" ;
-  int    nbTerme = 0 ;
-  
-  while (Serial.available() > 0) {
-    acquisDonnees = Serial.read() ;
-    delay(80) ;
-    
-    if ( acquisDonnees != 32 && acquisDonnees != 10 )  chaineCaractere = chaineCaractere + acquisDonnees  ;
-    else {
-      switch (nbTerme) {
-        case 0:
-          Serial.println("##### Reception de parametres #####");
-          chaineCaractere = "" ;
-          nbTerme++ ;
-          break;
-        case 1 :  // 1ere option  --> remontée du mode
-          mode = chaineCaractere ;
-          chaineCaractere = "" ;
-          nbTerme++ ;
-          switch (mode.toInt()) {
-            case 0: 
-              modeTexte = "Réduit" ;
-              break ;
-            case 3:
-              modeTexte = "Confort" ;
-              break ;
-            case 4:
-              modeTexte = "Hors gel" ;
-              break ;            
-          }
-          break ;  
-        case 2 :  // 2nd option  --> remontée de la temperature
-          temperature = chaineCaractere.toInt() ;
-          chaineCaractere = "" ;
-          nbTerme++ ;
-          break ;  
-       }
-    }
+  if (Serial.available() > 0) {
 
-    if ( nbTerme == 3 ) { 
-      Serial.println("Lancement commande avec parametres : ");
-      Serial.println("     Mode            --> " + mode + " - " + modeTexte );
-      Serial.println("     Temperature eau --> " + temperature + "°C"); 
-      commande(mode.toInt(),temperature.toInt()) ;
-      Serial.println("##### Commandes envoyées #####");
-      Serial.println("");
+    Serial.println("##### Réception d'une commande #####");
+
+    // Récup de l'input (ligne de commande)
+    chaineCaractere = Serial.readStringUntil('\n');;
+    delay(80);
+
+    // Récup des deux infos pertinentes de la commande texte
+    mode = getValue(chaineCaractere,' ',1);
+    temperature = getValue(chaineCaractere,' ',2);
+
+    // Interprétation & validité
+    modeTexte=invalide;
+    tempTexte=invalide;
+    switch (mode.toInt()) {
+      case 0:
+        modeTexte="Réduit";break;
+      case 3:
+        modeTexte="Confort";break;
+      case 4:
+        modeTexte="Hors Gel";break;
     }
-  }     
+    if (temperature.toInt()>=10 and temperature.toInt()<=100) tempTexte="ok";
+    Serial.println("    Mode   --> " + mode + " - " + modeTexte );
+    Serial.println("    T° eau --> " + temperature + "°C - " + tempTexte);
+
+    // Action en conséquence
+    if (tempTexte==invalide or modeTexte==invalide) {
+      // Commande invalide => on signale mais on ne fait rien
+      Serial.println("##### Commande invalide ignorée ! #####");
+    } else {
+      // Commande valide => génération et émission de la trame
+      Serial.println("Génération des trames...");
+      commande(mode.toInt(), temperature.toInt());
+      Serial.println("##### Commande traitée #####");
+    }
+    Serial.println("");
+  }
+}
+
+// Génération et émission d'une trame
+void commande(byte mode, byte temperature) {
+
+  for (byte x = 0; x < 3; x++) { // boucle de 3 messages
+
+    // Génération de la trame
+    // **********************
+
+    old_state = 0;
+
+    // Message 9 --> numéro de message 0 à 2
+    message[9] = x;
+
+    // Message : 10 -> mode
+    if (x == 2) message[10] = mode;
+    else message[10] = mode + 0x80;
+
+    // Message : 11 -> temperature
+    message[11] = temperature;
+
+    // Messages 13 et 14 -> checksum
+    int checksum = 0;
+    for (int i = 4; i <= 12; i++) {
+      checksum -= message[i];
+    }
+    message[13] = highByte(checksum);
+    message[14] = lowByte(checksum);
+
+    Serial.print("Trame ");
+    Serial.print(x);
+    Serial.print(" : ");
+
+    for (int i = 1; i <= 16; i++) {
+      if (message[i] <= 0x0F) Serial.print("0");
+      Serial.print(message[i], HEX);
+    }
+    Serial.println("");
+
+    // Emission de la trame
+    // ********************
+    for (int i = 1; i <= 16; i++) { // boucle de 16 bytes
+      conversion(message[i]);
+    }
+    digitalWrite(TRANSCEIVER_TX_PIN, LOW);
+    delay(33);
+  }
+  digitalWrite(TRANSCEIVER_TX_PIN, LOW);
+}
+
+// "Emission"
+// Pas complètement boîte aux lettres car il y a un bitstuffing à faire
+void conversion(byte input) {
+  for (byte n = 0; n < 8; n++) { // boucle pour chaque bit
+    // Emission proprement dite du bit
+    writeBit(bitRead(input, n));
+    // Bitstuffing
+    if (num_byte >= 4 && num_byte <= 14) {
+      if (bitRead(input, n) == 1) bitstuff++; // incrémente le compteur bitstuffing
+    }
+    if (bitRead(input, n) == 0) bitstuff = 0;
+    if (bitstuff >= 5) {
+      writeBit(0);
+      bitstuff = 0;
+    }
+  }
+}
+
+// Emission bit à bit bas niveau
+void writeBit(bool Bit) {
+  old_state = !old_state;
+  digitalWrite(TRANSCEIVER_TX_PIN, old_state);
+  delayMicroseconds(long_pulse);
+  if (Bit) {
+    old_state = !old_state;
+    digitalWrite(TRANSCEIVER_TX_PIN, old_state);}
+
+  else {
+    digitalWrite(TRANSCEIVER_TX_PIN, old_state);
+  }
+  delayMicroseconds(long_pulse);
+}
+
+// Récup du n-ème mot dans un string (n=index)
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {
+    0,
+    -1
+  };
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
